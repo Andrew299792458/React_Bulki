f4a01e9ca95946ca2dba4b86949eef867972dc4a
Andrew299792458 <mr.andrew793@gmail.com>
2023-01-07 20:59:44 +0400

    plus-minus

--- react-pur/src/components/header/index.jsx
+++ react-pur/src/components/header/index.jsx
@@ -2,9 +2,24 @@ import React from "react";
 import { Component } from "react";
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            flag: 0,
+        }
+    }
+    Plus = () => {
+        this.setState({ flag: ++this.state.flag })
+    }
+    Minus = () => {
+        this.setState({ flag: --this.state.flag })
+    }
     render() {
+        console.log(this.state.flag);
         return <>
-            <p>text</p>
+            <p>{this.state.flag}</p>
+            <button onClick={this.Plus}>+</button>
+            <button onClick={this.Minus}>-</button>
         </>
     }
 }
