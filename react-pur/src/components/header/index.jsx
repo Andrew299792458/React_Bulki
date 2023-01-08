ce36b7cbb444058b96cdc0aa8ab2a5f74a531264
Andrew299792458 <mr.andrew793@gmail.com>
2023-01-08 20:33:16 +0400

    last-minus-plus

--- react-pur/src/components/header/index.jsx
+++ react-pur/src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Component } from "react";
+import styles from "./styles.css"
 
 class Header extends Component {
     constructor(props) {
@@ -8,18 +9,33 @@ class Header extends Component {
             flag: 0,
         }
     }
+    Inp = (e) => {
+        this.setState({ num: e.target.value })
+    }
+
     Plus = () => {
-        this.setState({ flag: ++this.state.flag })
+        this.setState({ flag: +this.state.num + this.state.flag })
     }
     Minus = () => {
-        this.setState({ flag: --this.state.flag })
+        if (this.state.flag > 0) {
+            this.setState({
+                flag: this.state.flag - +this.state.num
+            })
+        }
+        if (+this.state.num > this.state.flag) {
+            this.setState({
+                flag: this.state.flag - this.state.flag
+            })
+
+        }
     }
     render() {
-        console.log(this.state.flag);
+
         return <>
             <p>{this.state.flag}</p>
             <button onClick={this.Plus}>+</button>
             <button onClick={this.Minus}>-</button>
+            <input type="number" onChange={this.Inp} className="red" />
         </>
     }
 }
