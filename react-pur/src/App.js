91206b6560a5d3748b20771e9cbcd9120d4114d5
Andrew299792458 <mr.andrew793@gmail.com>
2023-01-10 21:01:53 +0400

    lLastVersionOfForm

--- react-pur/src/App.js
+++ react-pur/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./components/header"
+import ValidateForm from "./components/ValidateForm"
 
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <ValidateForm />
     </div>
   );
 }
